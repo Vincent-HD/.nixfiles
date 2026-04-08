46f3c491f14ab30a0ff67b22069419466557e898
Vincent-HD <vincenthoudan@gmail.com>
2026-04-08 20:30:51 +0200

    feat: set max nix genetation kept to 10

--- hosts/pc-fixe/configuration.nix
+++ hosts/pc-fixe/configuration.nix
@@ -11,6 +11,7 @@
 
       boot.loader.systemd-boot.enable = true;
       boot.loader.efi.canTouchEfiVariables = true;
+      boot.loader.systemd-boot.configurationLimit = 10;
 
       networking.hostName = "pc-fixe";
       networking.networkmanager.enable = true;
